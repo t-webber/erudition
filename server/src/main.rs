1eafb71fc7160e89c1bdc7a8c7d5b5684bbd9297
Tom Webber <t.c.w.webber@gmail.com>
2026-03-14 22:21:30 +0100

    feat(server): add way to update list

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,10 @@
 use std::sync::Mutex;
 
-use actix_web::web::Data;
-use actix_web::{App, HttpResponse, HttpServer, Responder, get};
-use serde::Serialize;
+use actix_web::web::{Data, Json};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post};
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 enum Item {
     MultipleChoice {
         question: String,
@@ -20,12 +20,23 @@ async fn list(items: Data<Items>) -> impl Responder {
     HttpResponse::Ok().json(items)
 }
 
+#[post("/add")]
+async fn add(item: Json<Item>, items: Data<Items>) -> impl Responder {
+    items.0.lock().unwrap().push(item.into_inner());
+    HttpResponse::Ok()
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let items = Data::new(Items::default());
 
-    HttpServer::new(move || App::new().app_data(items.clone()).service(list))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .app_data(items.clone())
+            .service(list)
+            .service(add)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
