97a06335282b831ac68336669561f67370894ee3
Tom Webber <t.c.w.webber@gmail.com>
2026-03-14 18:44:25 +0100

    chore(clippy): add lint configuration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+//! An android app.
+#![warn(
+    missing_docs,
+    warnings,
+    deprecated_safe,
+    future_incompatible,
+    keyword_idents,
+    let_underscore,
+    nonstandard_style,
+    refining_impl_trait,
+    rust_2018_compatibility,
+    rust_2018_idioms,
+    rust_2021_compatibility,
+    rust_2024_compatibility,
+    unused,
+    clippy::all,
+    clippy::pedantic,
+    clippy::style,
+    clippy::perf,
+    clippy::complexity,
+    clippy::correctness,
+    clippy::restriction,
+    clippy::nursery,
+//     clippy::cargo
+)]
+#![allow(
+    clippy::single_call_fn,
+    clippy::implicit_return,
+    clippy::pattern_type_mismatch,
+    clippy::blanket_clippy_restriction_lints,
+    clippy::missing_trait_methods,
+    clippy::question_mark_used,
+    clippy::mod_module_files,
+    clippy::module_name_repetitions,
+    clippy::pub_with_shorthand,
+    clippy::unseparated_literal_suffix,
+    clippy::else_if_without_else,
+    reason = "style only"
+)]
+
 use dioxus::core::Element;
 use dioxus::dioxus_core;
 use dioxus::prelude::{component, rsx};
