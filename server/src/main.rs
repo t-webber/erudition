4b5febd9da25b3f1c6891f63b36c301f99041926
Tom Webber <t.c.w.webber@gmail.com>
2026-03-14 22:00:19 +0100

    feat(server): add actix boilerplate

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+
+#[get("/list")]
+async fn list() -> impl Responder {
+    HttpResponse::Ok().json("Hello world!\n")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(list))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
