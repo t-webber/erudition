ee07f39dedb1e1bd2e91bfe4cc8ceba296ad8582
Tom Webber <t.c.w.webber@gmail.com>
2026-03-15 07:21:19 +0100

    feat(server): store items to disk

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,12 @@
+use std::fs;
+use std::io;
 use std::sync::Mutex;
 
 use actix_web::web::{Data, Json};
-use actix_web::{App, HttpResponse, HttpServer, Responder, get, post};
+use actix_web::{App, HttpResponse, HttpServer, get, post};
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 enum Item {
     MultipleChoice {
         question: String,
@@ -12,18 +14,41 @@ enum Item {
     },
 }
 
-#[derive(Default, Serialize)]
+#[derive(Default, Serialize, Debug)]
 struct Items(Mutex<Vec<Item>>);
 
+enum SaveError {
+    Io(io::Error),
+    Serialisation(postcard::Error),
+}
+
+impl Items {
+    fn save(&self) -> Result<(), SaveError> {
+        let data = postcard::to_allocvec(&self).map_err(SaveError::Serialisation)?;
+        fs::write("data", data).map_err(SaveError::Io)?;
+        Ok(())
+    }
+}
+
 #[get("/list")]
-async fn list(items: Data<Items>) -> impl Responder {
+async fn list(items: Data<Items>) -> HttpResponse {
     HttpResponse::Ok().json(items)
 }
 
 #[post("/add")]
-async fn add(item: Json<Item>, items: Data<Items>) -> impl Responder {
+async fn add(item: Json<Item>, items: Data<Items>) -> HttpResponse {
     items.0.lock().unwrap().push(item.into_inner());
-    HttpResponse::Ok()
+    match items.save() {
+        Ok(()) => HttpResponse::Ok().into(),
+        Err(SaveError::Serialisation(ser)) => {
+            eprintln!("Failed to serialise items to disk:\nItems:\n{items:?}\n\nError:\n{ser}");
+            HttpResponse::UnprocessableEntity().body(format!("Failed to serialise data: {ser}"))
+        }
+        Err(SaveError::Io(io)) => {
+            eprintln!("Failed to save items to disk: {io}");
+            HttpResponse::InternalServerError().into()
+        }
+    }
 }
 
 #[actix_web::main]
