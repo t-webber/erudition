3767cb7cbcf1c450f8d0cb5f755b168d609f0090
Tom Webber <t.c.w.webber@gmail.com>
2026-04-27 08:43:39 +0200

    chore(proc-macro): lint proc-macro crate with the same lints

--- proc-macro/src/lib.rs
+++ proc-macro/src/lib.rs
@@ -1,21 +1,52 @@
-use std::sync::Mutex;
+//! Proc-macro crate to create proc-macros for erudition.
+
+use std::sync::{Mutex, MutexGuard};
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{Error, Ident, ItemFn, LitStr, Pat, parse};
+use syn::{Error, FnArg, Ident, ItemFn, LitStr, Pat, parse};
 
 thread_local! {
     static ROUTES: Mutex<Vec<String>> = Mutex::default();
 }
 
-fn error(attr: TokenStream, msg: &str) -> TokenStream {
-    Error::new_spanned(proc_macro2::TokenStream::from(attr), msg)
-        .to_compile_error()
-        .into()
+/// Wrapper to create proc-macros for the request handling routines from
+/// `actix_web` (e.g. `get`, `post`, `put`, etc.).
+macro_rules! macros {
+    ($($request:ident),*) => {
+        $(
+            #[proc_macro_attribute]
+            #[doc = concat!("Wrapper of the ", stringify!($request), " actix request, with automatic registration through `routes!` and automatic logging")]
+            pub fn $request(arg: TokenStream, body: TokenStream) -> TokenStream {
+            logged_actix_request(&quote!($request).into(), arg, body)
+            }
+        )*
+    };
 }
 
+/// Gives a mutable pointer to the [`ROUTES`] static list of registered routes.
+fn get_routes<T, F: Fn(&mut MutexGuard<'_, Vec<String>>) -> T>(mtd: F) -> T {
+    ROUTES.with(|routes| match routes.lock() {
+        Ok(mut vec) => mtd(&mut vec),
+        Err(mut poison) => mtd(poison.get_mut()),
+    })
+}
+
+macros!(get, post, put);
+
+/// Fires a nice compilation error when the macro is not used correctly.
+fn error(attr: impl Into<proc_macro2::TokenStream>, msg: &str) -> TokenStream {
+    Error::new_spanned(attr.into(), msg).to_compile_error().into()
+}
+
+/// Wrapper to create proc-macros for the request handling routines from
+/// `actix_web` (e.g. `get`, `post`, `put`, etc.).
+///
+/// # Panics
+///
+/// If the proc-macro is used incorrectly.
 fn logged_actix_request(
-    request: proc_macro2::TokenStream,
+    request: &proc_macro2::TokenStream,
     arg: TokenStream,
     body: TokenStream,
 ) -> TokenStream {
@@ -34,38 +65,16 @@ fn logged_actix_request(
         .sig
         .inputs
         .iter()
-        .filter_map(|input| {
-            if let syn::FnArg::Typed(pat_type) = input
-                && let Pat::Ident(pat_ident) = *pat_type.pat.clone()
-            {
-                let ident = pat_ident.ident;
-                let string = ident.to_string();
-                if string == "state" {
-                    None
-                } else {
-                    Some(Ok(quote!(
-                        log.push_str(&format!(" [{}: {:?}]",
-                            #string,
-                            &#ident
-                        ));
-                    )))
-                }
-            } else {
-                Some(Err(error(
-                    quote!(#input).into(),
-                    "invalid function argument",
-                )))
-            }
-        })
+        .filter_map(log_param)
         .collect::<Result<Vec<_>, _>>()
     {
         Ok(args) => args,
         Err(error) => return error,
     };
 
-    ROUTES.with(|v| v.lock().unwrap().push(name.to_string()));
+    get_routes(|routes| routes.push(name.to_string()));
 
-    let expanded = quote! {
+    quote! {
         #[allow(clippy::literal_string_with_formatting_args)]
         #[actix_web::#request(#path)]
         #sig {
@@ -75,37 +84,50 @@ fn logged_actix_request(
             state.log(&format!("\x1b[36m{:4} {} ({}){}\x1b[0m", stringify!(#request), res.status().as_u16(), #path, &log));
             res
         }
-    };
-
-    expanded.into()
+    }.into()
 }
 
-macro_rules! macros {
-    ($($request:ident),*) => {
-        $(
-            #[proc_macro_attribute]
-            pub fn $request(arg: TokenStream, body: TokenStream) -> TokenStream {
-            logged_actix_request(quote!($request).into(), arg, body)
-            }
-        )*
-    };
+/// Adds the value of a parameter to the logs.
+fn log_param(
+    input: &FnArg,
+) -> Option<Result<proc_macro2::TokenStream, TokenStream>> {
+    if let syn::FnArg::Typed(pat_type) = input
+        && let Pat::Ident(pat_ident) = *pat_type.pat.clone()
+    {
+        let ident = pat_ident.ident;
+        let string = ident.to_string();
+        if string == "state" {
+            return None;
+        }
+        Some(Ok(quote!(
+            log.push_str(&format!(" [{}: {:?}]",
+                #string,
+                &#ident
+            ));
+        )))
+    } else {
+        Some(Err(error(quote!(#input), "invalid function argument")))
+    }
 }
 
-macros!(get, post, put);
-
+/// Registers all the routes at once, to not forget to register them once
+/// defined.
+///
+/// # Panics
+///
+/// If the proc-macro is used incorrectly.
 #[proc_macro]
 pub fn routes(app: TokenStream) -> TokenStream {
     let Ok(ident): Result<Ident, _> = parse(app.clone()) else {
         return error(app, "argument should be an identifier");
     };
 
-    let stmts: Vec<_> = ROUTES.with(|v| {
-        v.lock()
-            .unwrap()
+    let stmts: Vec<_> = get_routes(|routes| {
+        routes
             .iter()
             .map(|name| {
-                let ident = Ident::new(name, proc_macro2::Span::call_site());
-                quote!(#ident)
+                let function = Ident::new(name, proc_macro2::Span::call_site());
+                quote!(#function)
             })
             .map(|id| quote!(.service(#id)))
             .collect()
